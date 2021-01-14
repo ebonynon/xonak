cc300b4b2ad55e0030a6a8e58bcfe5505dcae21f
ebonynon <sithumsandeepap@gmail.com>
2021-01-14 21:02:27 +0530

    Set naming

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Header from "./Header";
-import Bannerpart from "./sec_banner_part";
-import Ctapart from "./sec_cta_part";
-import Servicepart from "./sec_service_part";
-import Aboutpart from "./sec_about_part";
-import Ourlatestwork from "./sec_our_latest_work";
+import BannerPart from "./sec_banner_part";
+import CtaPart from "./sec_cta_part";
+import ServicePart from "./sec_service_part";
+import AboutPart from "./sec_about_part";
+import OurLatestWork from "./sec_our_latest_work";
 import Footer from "./footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <Bannerpart />
-      <Ctapart />
-      <Servicepart />
-      <Aboutpart />
-      <Ourlatestwork />
-      <Ourlatestwork />
+      <BannerPart />
+      <CtaPart />
+      <ServicePart />
+      <AboutPart />
+      <OurLatestWork />
       <Footer />
     </div>
   );
