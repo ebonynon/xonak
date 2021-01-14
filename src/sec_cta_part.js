e564553aa34063e70120fdac6f05567597d1b30b
ebonynon <sithumsandeepap@gmail.com>
2021-01-14 19:51:56 +0530

    class to function

--- src/sec_cta_part.js
+++ src/sec_cta_part.js
@@ -1,20 +1,16 @@
-import React from 'react';
-
-export default class Cta_part extends React.Component {
-    render() {
-      return (
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <div class="cta_part_iner">
-                        <div class="cta_part_text">
-                            <p> Our Features</p>
-                            <h1>Building brands with purpose</h1>
-                        </div>
-                    </div>
-                </div>
+export default function Cta_part() {
+  return (
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-12">
+          <div class="cta_part_iner">
+            <div class="cta_part_text">
+              <p> Our Features</p>
+              <h1>Building brands with purpose</h1>
             </div>
+          </div>
         </div>
-      );
-    }
+      </div>
+    </div>
+  );
 }
