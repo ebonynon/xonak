26d0dd54c96028808d852a712c8aa83f3d1964f8
ebonynon <sithumsandeepap@gmail.com>
2021-01-14 20:48:57 +0530

    Remove _ on naming

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import Header from "./Header";
-import Banner_part from "./sec_banner_part";
-import Cta_part from "./sec_cta_part";
-import Service_part from "./sec_service_part";
-import About_part from "./sec_about_part";
-import Our_latest_work from "./sec_our_latest_work";
+import Bannerpart from "./sec_banner_part";
+import Ctapart from "./sec_cta_part";
+import Servicepart from "./sec_service_part";
+import Aboutpart from "./sec_about_part";
+import Ourlatestwork from "./sec_our_latest_work";
 import Footer from "./footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <Banner_part />
-      <Cta_part />
-      <Service_part />
-      <About_part />
-      <Our_latest_work />
-      <Our_latest_work />
+      <Bannerpart />
+      <Ctapart />
+      <Servicepart />
+      <Aboutpart />
+      <Ourlatestwork />
+      <Ourlatestwork />
       <Footer />
     </div>
   );
